a7e6ff2e7f2bf67a0a6ffbddf08f5c046db73d8a
hasanmdabid <abid1084@gmail.com>
2024-02-16 16:32:59 +0100

    Setting the master branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This script will be the main python script in the project. 
+# This project have two branch in gihub. The main branch will be the project final version. 
+# Whiles the master branch will be the trial version.
 if __name__ == "__main__":
     print("Hello, World!")
     
